a33fda7d31a79316622a8058fda016351c2f63b2
kieferro <81773954+kieferro@users.noreply.github.com>
2021-08-28 14:18:03 +0200

    The name of the user is now found by the user_tag id and not class names

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ browser.runtime.onMessage.addListener(function (request) {
         console.log(text);
         console.log(name === text);
 
-        if (name === text){
+        if (name === text) {
             browser.runtime.sendMessage({code: 2});
             return;
         }
@@ -30,7 +30,7 @@ browser.runtime.onMessage.addListener(function (request) {
         }
         document.getElementsByClassName("copyable autoselect")[1].value = request.pgn;
         document.getElementsByClassName("button button-thin action text")[0].click();
-    } else if (request.code === 3){
+    } else if (request.code === 3) {
         window.close();
     }
 });
@@ -221,15 +221,13 @@ function addFollowing() {
     if (buttons[0].childNodes.length > 5) {
         return;
     }
-    let name = document.getElementsByClassName("dasher")[0].childNodes[0].textContent;
-    console.log(name);
+    let name = document.getElementById("user_tag").textContent;
 
-    let button = buttons[0].childNodes[0].childNodes[0];
-    let new_node = button.cloneNode();
+    let new_node = buttons[0].childNodes[0].childNodes[0].cloneNode();
     new_node.dataset.icon = "⛹";
     new_node.href = "https://lichess.org/@/" + name + "/following";
-    buttons[0].insertBefore(new_node, document.getElementsByClassName("dasher")[0]);
 
+    buttons[0].insertBefore(new_node, document.getElementById("user_tag").parentNode);
 }
 
 setTimeout(addFollowing, 10);
@@ -257,8 +255,7 @@ function get_pgn() {
 }
 
 function error(error) {
-    console.log("Es ist ein Fehler aufgetreten.");
-    console.log(error);
+    console.log("Error:", error);
 }
 
 function gotRatings(item) {
