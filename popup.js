60d7012ac44b1417783b5b674c6a02728335a28f
kieferro <81773954+kieferro@users.noreply.github.com>
2021-12-24 17:14:01 +0100

    Revising the popup-script: Changing storing of local variables and manipulation of color codes

--- popup.js
+++ popup.js
@@ -1,73 +1,44 @@
 const messages = ["Deaktiviert", "Beim Spielen", "Immer"];
 let ratings = 0;
 
-function error(error) {
-    console.log("Es ist ein Fehler aufgetreten.");
-    console.log(error);
-}
-
-function gotReport(item) {
-    if (item.report !== undefined) {
-        document.getElementById("report").checked = item.report;
-    }
-}
-
-function gotAnalyse(item) {
-    if (item.analyse !== undefined) {
-        document.getElementById("analyse").checked = item.analyse;
-    }
-}
-
-function gotDuell(item) {
-    if (item.duell !== undefined) {
-        document.getElementById("duell").checked = item.duell;
-    }
-}
-
-function gotRatings(item) {
-    if (item.ratings !== undefined) {
-        ratings = item.ratings;
-        set_state(false);
-    }
-}
-
-
-function new_values() {
-    let report = document.getElementById("report").checked;
-    let duell = document.getElementById("duell").checked;
-    let analyse = document.getElementById("analyse").checked;
-    browser.storage.local.set({report, duell, analyse, ratings});
-}
-
-function set_state(increment) {
-    if (increment) {
-        ratings = (ratings + 1) % 3;
-    }
-
-    let button = document.getElementById("ratings");
-    button.textContent = messages[ratings];
-    button.className = "change-button" + (ratings + 1).toString();
-}
-
-function clicked() {
-    set_state(true);
-    new_values();
-}
-
 function open_analysis() {
-    browser.runtime.sendMessage({code: 0});
+    console.log("Opening analysis");
+    window.close();
+
+    // browser.runtime.sendMessage({code: 0});
 }
 
-function start() {
-    document.getElementById("ratings").addEventListener("click", clicked, false);
-    document.getElementById("duell").addEventListener("click", new_values, false);
-    document.getElementById("report").addEventListener("click", new_values, false);
-    document.getElementById("analyse").addEventListener("click", new_values, false);
-    document.getElementById("open-analysis").addEventListener("click", open_analysis, false);
-    browser.storage.local.get("report").then(gotReport, error);
-    browser.storage.local.get("duell").then(gotDuell, error);
-    browser.storage.local.get("ratings").then(gotRatings, error);
-    browser.storage.local.get("analyse").then(gotAnalyse, error);
+let preferences = {"toggles": [true, true, true, true], "ratings": 0};
+
+function readPreferences() {
+    let toggles = [];
+    let slider = document.getElementsByClassName("checkbox");
+
+    for (let i = 0; i < slider.length; i++) {
+        toggles.push(slider[i].checked);
+    }
+    console.log("Set", toggles);
 }
 
-document.addEventListener("DOMContentLoaded", start, false);
+function gotPreferences(item) {
+    if (item.signature) {
+        preferences = item;
+    }
+}
+
+function gotError(_) {
+    jQuery("body").css({"background-color": "red"});
+}
+
+function setup() {
+    browser.storage.local.get("pref").then(gotPreferences, gotError);
+    jQuery(".slider").on("click", function () {
+        setTimeout(readPreferences, 100)
+    });
+}
+
+document.addEventListener("DOMContentLoaded", setup, false);
+
+
+//let pref = {"a": 1, "b": 3};
+//browser.storage.local.set({pref});
