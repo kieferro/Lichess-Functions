2cac9a2ab4cef2943d09c589b0db1706c9b275f8
kieferro <81773954+kieferro@users.noreply.github.com>
2021-12-25 02:27:59 +0100

    Finished logging of preferences for toggles

--- popup.js
+++ popup.js
@@ -1,14 +1,5 @@
 const messages = ["Deaktiviert", "Beim Spielen", "Immer"];
-let ratings = 0;
-
-function open_analysis() {
-    console.log("Opening analysis");
-    window.close();
-
-    // browser.runtime.sendMessage({code: 0});
-}
-
-let preferences = {"toggles": [true, true, true, true], "ratings": 0};
+let preferences = {"toggles": [true, true, true, true], "ratings": 0, "signature": true};
 
 function readPreferences() {
     let toggles = [];
@@ -17,28 +8,44 @@ function readPreferences() {
     for (let i = 0; i < slider.length; i++) {
         toggles.push(slider[i].checked);
     }
-    console.log("Set", toggles);
+    preferences.toggles = toggles;
+    browser.storage.local.set({preferences});
+}
+
+function applyPreferences() {
+    let slider = document.getElementsByClassName("checkbox");
+
+    for (let i = 0; i < slider.length; i++) {
+        slider[i].checked = preferences["toggles"][i];
+    }
 }
 
 function gotPreferences(item) {
+    item = item.preferences;
+
     if (item.signature) {
         preferences = item;
     }
+    applyPreferences();
 }
 
 function gotError(_) {
     jQuery("body").css({"background-color": "red"});
 }
 
+function open_analysis() {
+    console.log("Opening analysis");
+    // browser.runtime.sendMessage({code: 0});
+}
+
 function setup() {
-    browser.storage.local.get("pref").then(gotPreferences, gotError);
+    browser.storage.local.get("preferences").then(gotPreferences, gotError);
+
     jQuery(".slider").on("click", function () {
         setTimeout(readPreferences, 100)
     });
+
+    jQuery("#open-analysis").on("click", open_analysis);
 }
 
 document.addEventListener("DOMContentLoaded", setup, false);
-
-
-//let pref = {"a": 1, "b": 3};
-//browser.storage.local.set({pref});
