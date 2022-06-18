c3394e6fef4f2c28cc5bfb0db60fcb08ddc43ae6
kieferro <81773954+kieferro@users.noreply.github.com>
2022-06-18 18:22:44 +0200

    Added preferences toggle

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let pressedButton = false;
 let timeSpan = 1;
 let deactivated = new Set([]);
 
-let preferences = {"toggles": [false, false, false, false, false, false], "ratings": 1, "signature": true};
+let preferences = {"toggles": [false, false, false, false, false, false, false], "ratings": 1, "signature": true};
 // Default config for MutationObserver
 const mutationConfig = {attributes: true, childList: true, subtree: true};
 const LIGHTBLUE = "rgb(140, 216, 230)";
